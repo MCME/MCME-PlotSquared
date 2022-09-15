61098315db7708f6c74e49433af6799ce0a5fd24
barteldvn <barteldvn@gmail.com>
2022-09-15 23:42:53 +0200

    - Some logic for claiming plots - Some logic for protecting plots that are being rated

--- src/main/java/com/mcmiddleearth/plotsquared/listener/P2CommandListener.java
+++ src/main/java/com/mcmiddleearth/plotsquared/listener/P2CommandListener.java
@@ -6,16 +6,14 @@ import com.mcmiddleearth.plotsquared.review.ReviewAPI;
 import com.mcmiddleearth.plotsquared.review.ReviewPlayer;
 import com.plotsquared.core.PlotAPI;
 import com.plotsquared.core.configuration.caption.TranslatableCaption;
-import com.plotsquared.core.events.PlotClearEvent;
-import com.plotsquared.core.events.PlotDeleteEvent;
-import com.plotsquared.core.events.PlotDoneEvent;
-import com.plotsquared.core.events.Result;
+import com.plotsquared.core.events.*;
+import com.plotsquared.core.player.PlotPlayer;
 import com.plotsquared.core.plot.Plot;
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
 
 
-public class P2CommandListener implements Listener {
+public class P2CommandListener {
 
     public P2CommandListener() {
         PlotAPI api = new PlotAPI();
@@ -26,6 +24,22 @@ public class P2CommandListener implements Listener {
     public void onPlotDone(PlotDoneEvent plotDoneEvent) {
         ReviewPlayer reviewPlayer = ReviewAPI.getReviewPlayer(Bukkit.getPlayer(plotDoneEvent.getPlot().getOwner()));
         plotDoneEvent.setEventResult(Result.DENY);
+    }
+
+    @Subscribe
+    public void onPlayerPlotClaim(PlayerClaimPlotEvent playerClaimPlotEvent) {
+        PlotPlayer plotPlayer = playerClaimPlotEvent.getPlotPlayer();
+        Player player = Bukkit.getPlayer(plotPlayer.getUUID());
+        ReviewPlayer reviewPlayer = ReviewAPI.getReviewPlayer(player);
+        if(plotPlayer.getAllowedPlots() > plotPlayer.getPlotCount()) {
+            for (Plot plot : playerClaimPlotEvent.getPlotPlayer().getPlots()) {
+                if (ReviewStatusFlag.isBeingReviewed(plot)) {
+                    reviewPlayer.sendMessage(TranslatableCaption.of("mcme.review.error.plot_being_reviewed_edit"));
+                    playerClaimPlotEvent.setEventResult(Result.DENY);
+                    return;
+                }
+            }
+        }
     }
 
     @Subscribe
